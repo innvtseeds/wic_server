d5c29abbae1e455c968c20a733613f5ef03910e0
sreerag-rajan <sreerag.rajan5@gmail.com>
2023-11-07 23:16:24 +0530

    started working on User

--- internal/repository/User.repository.go
+++ internal/repository/User.repository.go
@@ -1,6 +1,14 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/innvtseeds/wdic-server/internal/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type UserRepository struct {
 	collection *mongo.Collection
@@ -11,4 +19,21 @@ func NewUserRepository(db *mongo.Database) *UserRepository {
 	return &UserRepository{
 		collection: collection,
 	}
+}
+
+func (r *UserRepository) Create(user *model.User) (*model.User, error) {
+	result, err := r.collection.InsertOne(context.Background(), user)
+	if err != nil {
+		//* @sreerag: NEED TO CHECK IF LOG.FATAL EXITS THE PROGRAM
+		//* IF SO THIS NEEDS TO BE AVOIDED AND INSTEAD JUST HAVE LOGGING BE DONE
+		log.Fatal("ERROR IN USER INSERT", err)
+		return nil, errors.New("Insert to Collection failed")
+	}
+
+	createdUser := &model.User{
+		Id:    result.InsertedID.(primitive.ObjectID),
+		Email: user.Email,
+	}
+
+	return createdUser, nil
 }
