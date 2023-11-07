016fa03dd603ad6a3aa9fe7d6c0e14978cc69792
sreerag-rajan <sreerag.rajan5@gmail.com>
2023-11-07 23:09:46 +0530

    started working on User

--- internal/model/User.model.go
+++ internal/model/User.model.go
@@ -1,1 +1,9 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email    string             `json:"email,omitempty"`
+	Password string             `json:"password,omitempty"`
+}
--- internal/repository/User.repository.go
+++ internal/repository/User.repository.go
@@ -1,1 +1,14 @@
 package repository
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type UserRepository struct {
+	collection *mongo.Collection
+}
+
+func NewUserRepository(db *mongo.Database) *UserRepository {
+	collection := db.Collection("user")
+	return &UserRepository{
+		collection: collection,
+	}
+}
